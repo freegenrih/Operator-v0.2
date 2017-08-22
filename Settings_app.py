9948b136e031e914f4924bee184c225fe2a6aa34
freegenrih <freegenrih@mail.ru>
2017-08-22 22:20:41 +0300

    manage.py mysql drop create db

--- Settings_app.py
+++ Settings_app.py
@@ -2,8 +2,8 @@ config_db = {
     'user': 'root',
     'password': 'password',
     'host': 'localhost',
-    'db': 'BigTester',
+    'db': 'Pult',
     'charset': 'utf8mb4'
     }
 
-key = 'thisIsSecretKey:)'
+key = 'ThisIsSecretKey:)'
--- manage.py
+++ manage.py
@@ -2,37 +2,35 @@ from sqlrw import wraper_write
 
 
 def drop_db():
-    sql = "DROP TABLE IPSenderData;" \
-          "DROP TABLE IPSender; " \
-          "DROP TABLE LogerData; " \
-          "DROP TABLE Users;"
+    sql = "DROP TABLE dbo_operator_application;" \
+          "DROP TABLE dbo_users; " \
+          "DROP TABLE user_type; "
     wraper_write(sql)
     print('Database Delete!!!')
 
 
 def create_db():
-    sql="CREATE TABLE IF NOT EXISTS `IPSender` ( `id` int(11) NOT NULL AUTO_INCREMENT," \
-        "`name_ipsender` text NOT NULL, `key_ipsender` text NOT NULL, PRIMARY KEY (`id`) ) " \
-        "ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8; " \
-        "CREATE TABLE IF NOT EXISTS `IPSenderData` ( `id` int(11) NOT NULL AUTO_INCREMENT, `kay` text NOT NULL, " \
-        "`ch1` int(11) NOT NULL, `ch2` int(11) NOT NULL, `ch3` int(11) NOT NULL, `ch4` int(11) NOT NULL, `ch5` int(11) " \
-        "NOT NULL, `ch6` int(11) NOT NULL, `ch7` int(11) NOT NULL, `ch8` int(11) NOT NULL, `datetimes` text NOT NULL, " \
-        "PRIMARY KEY (`id`) ) ENGINE=InnoDB AUTO_INCREMENT=176 DEFAULT CHARSET=utf8;" \
-        "CREATE TABLE IF NOT EXISTS `LogerData` ( `id` int(11) NOT NULL AUTO_INCREMENT, `name` text NOT NULL, " \
-        "`idsender` text NOT NULL, `ch1` int(4) NOT NULL, `ch2` int(4) NOT NULL, `ch3` int(4) NOT NULL, `ch4` int(4) " \
-        "NOT NULL, `ch5` int(4) NOT NULL, `ch6` int(4) NOT NULL, `ch7` int(4) NOT NULL, `ch8` int(4) NOT NULL, " \
-        "`datatime` text NOT NULL, PRIMARY KEY (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8;" \
-        "CREATE TABLE IF NOT EXISTS `Users` ( `id` int(11) NOT NULL AUTO_INCREMENT, `username` text NOT NULL, " \
-        "`email` text NOT NULL, `password` text NOT NULL, `statusadmin` tinyint(1) NOT NULL, PRIMARY KEY (`id`) ) " \
-        "ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8;" \
-        "INSERT INTO `BigTester`.`Users` (`id`, `username`, `email`, `password`, `statusadmin`) " \
-        "VALUES  (1, 'Gennadiy', 'admin@admin.ru', 'admins', 1);"
+    sql="CREATE TABLE `dbo_operator_application` (`id` int(11) NOT NULL AUTO_INCREMENT, " \
+        "`date_of_creation` text NOT NULL, `name_operator` text NOT NULL, `name_pult` text NOT NULL," \
+        "`message` text NOT NULL, `date_of_completion` text NOT NULL, PRIMARY KEY (`id`)) " \
+        "ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;" \
+        "CREATE TABLE `dbo_users` (`id` int(11) NOT NULL AUTO_INCREMENT, `user_name` text NOT NULL, `user_password` " \
+        "text NOT NULL, `user_type` text NOT NULL, PRIMARY KEY (`id`)) " \
+        "ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;" \
+        "CREATE TABLE `user_type` (`id` int(2) NOT NULL AUTO_INCREMENT, `user_type` text NOT NULL, PRIMARY KEY (`id`))" \
+        "ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;" \
+        "INSERT INTO `Pult`.`dbo_users` (`id`, `user_name`, `user_password`, `user_type`) " \
+        "VALUES (1, 21, 'password', 'Admin');" \
+        "INSERT INTO `Pult`.`user_type` (`id`, `user_type`) " \
+        "VALUES (1, 'Admin'), (2, 'Operator'), (3, 'Engineer'), (4, 'Electrician');" \
+        "INSERT INTO `Pult`.`dbo_operator_application` (`id`, `date_of_creation`, `name_operator`, `name_pult`, " \
+        "`message`, `date_of_completion`) VALUES (1, '21-08-2017 18:00:00', 21, 'Грифон', 'Убрать из 220', '');"
     wraper_write(sql)
     print('Database Create!!!')
 
 
 if __name__ == '__main__':
-    enter_command = str(input('1 : Drop DB \n2 : Create Clean DB(user=admin@admin.ru password=admins)\n'))
+    enter_command = str(input('1 : Drop DB \n2 : Create Clean DB\n'))
     if enter_command == str(1):
         drop_db()
     elif enter_command == str(2):
