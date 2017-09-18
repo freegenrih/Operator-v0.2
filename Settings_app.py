6b186a43d05641d7122268be92dbe1c5e2451e54
freegenrih <freegenrih@mail.ru>
2017-09-18 20:57:40 +0300

    no remember

--- Settings_app.py
+++ Settings_app.py
@@ -11,7 +11,7 @@ key = 'ThisIsSecretKey:)'
 config_debug_local = {'host': 'localhost',
                       'debug': 'True'}
 
-config_debug_105 = {'host': '192.168.0.105',
+config_debug_105 = {'host': '192.168.0.102',
                     'debug': 'True'}
 
 config_debug_my_work = {'host': '192.168.100.181',
